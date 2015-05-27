554131e3c49eb9e555bd89b959fc6fd2c16ae4db
Vladimir Dmitriev <defurator@mail.ru>
2015-05-27 23:04:54 +0300

    newcatalog begin

--- Gruntfile.js
+++ Gruntfile.js
@@ -95,9 +95,6 @@ module.exports = function(grunt) {
     });
 
     // 3. Тут мы указываем Grunt, что хотим использовать этот плагин
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-sass');
     //grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-watch');
